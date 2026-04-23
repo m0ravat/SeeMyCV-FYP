7b020ce07cd8fa3cccdc154a91e593d722885323
Muhammad Ravat <m0ravat736@gmail.com>
2026-04-23 09:50:29 +0100

    updated stripe api

--- SeeMyCV/lib/stripe.ts
+++ SeeMyCV/lib/stripe.ts
@@ -2,5 +2,5 @@ import 'server-only';
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-03-31.basil',
+  apiVersion: '2026-03-25.dahlia',
 });
